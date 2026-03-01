a6fe1a17785e9653abe475231b2e44a920fabf8b
jeoitim <tim.rt.yip@outlook.com>
2026-03-01 12:03:44 +0800

    ``` chore(gui): 更新关于页面版本号
    更新关于页面中显示的应用版本号从 v0.1.2 beta 到 v0.1.3 beta
    ```

--- gui.py
+++ gui.py
@@ -8,7 +8,7 @@
 
 # 关于页信息变量
 ABOUT_CONTENT = """
-<h2>Fast Embed Sub v0.1.2 beta</h2>
+<h2>Fast Embed Sub v0.1.3 beta</h2>
 <p><img src="{icon}" width="128" height="128"></p>
 <p><b>作者:</b> Jeoitim Yip</p>
 <p><b>GitHub:</b> <a href="https://github.com/Jeoitim/fast-embed-sub">https://github.com/Jeoitim/fast-embed-sub</a></p>
